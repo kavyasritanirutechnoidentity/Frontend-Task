b45d6a3757cd5b9110a082f7ea7e2b869e781a69
kavyasritanirutechnoidentity <kavyasri.taniru@technoidentity.com>
2025-03-24 12:23:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Frontend-Task/',  
   plugins: [
     react(),
     tailwindcss(),
